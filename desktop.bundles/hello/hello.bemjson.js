252182bc9fde7d9f67f78df8eec4510a0e8c6210
vvs <aa151972@gmail.com>
2017-06-22 08:57:20 +0300

    first bem

---
+++ desktop.bundles/hello/hello.bemjson.js
@@ -0,0 +1,14 @@
+({
+    block : 'page',
+    title : 'hello',
+    head : [
+        { elem : 'css', url : 'hello.min.css' }
+    ],
+    scripts : [{ elem : 'js', url : 'hello.min.js' }],
+    mods : { theme : 'islands' },
+    content : [
+        {
+            block : 'hello'
+        }
+    ]
+})
